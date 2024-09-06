5900e3f84b55392b209ed4497ff8abb1405f4af0
Madushani Piyadasa <shanipiyadasa@gmail.com>
2024-09-06 09:42:04 +0300

    Age app started
    Age app first code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int age = 10;
+        if (age > 0 && age <18){
+
+System.out.println("You are Underage.");
+
+        }
+
+        //System.out.println("Hello, World!");
     }
 }
