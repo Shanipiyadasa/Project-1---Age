4d5c0282f3f7fcb744e581484f927855051dbf68
Madushani Piyadasa <shanipiyadasa@gmail.com>
2024-09-06 09:57:17 +0300

    age app 5th task
    5.a task completed

--- src/App.java
+++ src/App.java
@@ -1,13 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int age = 10;
+        int age = 18;
         if (age > 0 && age <18){
 
 System.out.println("You are Underage.");
+if (age >=15){
 
+System.out.println("You can drive a moped");
+
+}
+
+        } else if (age >= 65)
+        {
+System.out.println("You are Retired");
+
+        } else{
+        System.out.println("You are an Adult");
         }
+// Age 18
+if (age == 18){
 
-        //System.out.println("Hello, World!");
+System.out.println(" You can drive a car");
+
+}
+
+
+
     }
 }
